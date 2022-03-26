921f597d62a4fe48da8ab85e4c2c8f9f936944ee
Xsotic <joshuadanielsaja@gmail.com>
2022-03-26 20:40:46 +0800

    first commit

---
+++ app/src/main/java/com/example/saja/CreateStudent.java
@@ -0,0 +1,122 @@
+package com.example.saja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.content.Context;
+
+public class CreateStudent extends AppCompatActivity {
+    //global variables
+    Spinner spinner;
+    CheckBox male, female;
+    Button add;
+    EditText fname, lname, address;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_student);
+
+        ref();
+        spnr();
+
+        //validator for checkboxes
+        male.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!male.isChecked() && !female.isChecked()) {
+                    male.setChecked(true);
+                    female.setChecked(false);
+                }
+                if ( female.isChecked() && male.isChecked()){
+                    male.setChecked(true);
+                    female.setChecked(false);
+                }
+
+            }
+        });
+        female.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!female.isChecked() && !male.isChecked()) {
+                    female.setChecked(true);
+                    male.setChecked(false);
+                }
+                if ( female.isChecked() && male.isChecked()){
+                    female.setChecked(true);
+                    male.setChecked(false);
+                }
+
+            }
+        });
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(fname.getText().toString().isEmpty() || lname.getText().toString().isEmpty() || address.getText().toString().isEmpty() ||
+                spinner.getSelectedItem().toString().isEmpty()){
+                    Toast.makeText(CreateStudent.this, "Some fields are missing.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!male.isChecked() && !female.isChecked())
+                {
+                    Toast.makeText(CreateStudent.this, "Please select a gender.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Context context = v.getRootView().getContext();
+                ObjectStudent objectStudent = new ObjectStudent();
+                objectStudent.setFirstname(fname.getText().toString());
+                objectStudent.setLastname(lname.getText().toString());
+                objectStudent.setAddress(address.getText().toString());
+                objectStudent.setCourse(spinner.getSelectedItem().toString());
+                if(female.isChecked()){
+                    objectStudent.setGender("Female");
+                }
+                else if(male.isChecked()){
+                    objectStudent.setGender("Male");
+                }
+                boolean createSuccessful = new TableControllerStudent(context).create(objectStudent);
+                if(createSuccessful){
+                    Toast.makeText(context, "Student information was saved.", Toast.LENGTH_SHORT).show();
+                    fname.getText().clear();
+                    lname.getText().clear();
+                    address.getText().clear();
+                    male.setChecked(false);
+                    female.setChecked(false);
+                    spinner.setSelection(0);
+
+                }else{
+                    Toast.makeText(context, "Unable to save student information.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+    public void ref(){
+        spinner = findViewById(R.id.Course_Spinner);
+        //
+        male = findViewById(R.id.Male_CheckBox);
+        female = findViewById(R.id.Female_CheckBox);
+        //
+        fname = findViewById(R.id.Firstname_EditText);
+        lname =findViewById(R.id.Lastname_EditText);
+        address = findViewById(R.id.Address_EditText);
+        //
+        add = findViewById(R.id. Add_Button);
+    }
+    public void spnr(){
+        //creating a list of items for the spinner.
+        String[] items = new String[]{"","BSIT", "BSED", "BSCE", "BSMA", "BSCRIM", "BSNURSING"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        spinner.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/example/saja/ViewRecord.java
@@ -0,0 +1,127 @@
+package com.example.saja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class ViewRecord extends AppCompatActivity {
+    TextView counter;
+    Spinner spinner;
+    Button viewall, search;
+    EditText searchid;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_record);
+        ref();
+
+        countRecords();
+        viewall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                readRecords();
+            }
+        });
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(searchid.getText().toString().isEmpty()){
+                    Toast.makeText(ViewRecord.this, "Some fields are missing.", Toast.LENGTH_SHORT).show();
+                    return;
+                }else{
+                    int id=  Integer.parseInt(searchid.getText().toString());
+                    searchStudent(id);
+                }
+
+            }
+        });
+
+    }
+    public void ref(){
+        counter = findViewById(R.id.RecordCounter_textView);
+        viewall = findViewById(R.id.view_Button);
+        searchid = findViewById(R.id.SearchStudent_EditText);
+        search = findViewById(R.id.search_Button);
+
+
+    }
+    public void countRecords(){
+        int recordCount = new TableControllerStudent(this).count();
+        counter.setText(recordCount + " Records Found");
+    }
+    public void readRecords() {
+        LinearLayout linearLayoutRecords = (LinearLayout) findViewById(R.id.linearRecords);
+        linearLayoutRecords.removeAllViews();
+        List<ObjectStudent> students = new TableControllerStudent(this).read();
+        if (students.size() > 0) {
+            for (ObjectStudent obj : students) {
+
+                int id = obj.getId();
+                String studentFirstname = obj.getFirstname();
+                String studentLastname = obj.getLastname();
+                String studentAddress = obj.getAddress();
+                String studentGender = obj.getGender();
+                String studentCourse = obj.getCourse();
+                int y= students.size();
+                String textViewContents = id +" "+studentLastname + ", " +studentFirstname + "-\t "+ studentAddress +"-\t\t" + studentGender + "-\t " + studentCourse;
+                TextView textViewStudentItem= new TextView(this);
+                textViewStudentItem.setPadding(4, 16, 4, 16);
+                textViewStudentItem.setTextSize(20);
+                textViewStudentItem.setText(textViewContents);
+                textViewStudentItem.setTag(Integer.toString(id));
+                linearLayoutRecords.addView(textViewStudentItem);
+                textViewStudentItem.setOnLongClickListener(new OnClickEditRecord());
+            }
+        }
+        else {
+            TextView locationItem = new TextView(this);
+            locationItem.setPadding(8, 8, 8, 8);
+            locationItem.setText("No records yet.");
+            linearLayoutRecords.addView(locationItem);
+        }
+    }
+    public void spnr(){
+        //creating a list of items for the spinner.
+        String[] items = new String[]{"","BSIT", "BSED", "BSCE", "BSMA", "BSCRIM", "BSNURSING"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        spinner.setAdapter(adapter);
+    }
+    public void searchStudent(final int studentId){
+        final TableControllerStudent tableControllerStudent = new TableControllerStudent(this);
+        LinearLayout linearLayoutRecords = (LinearLayout) findViewById(R.id.linearRecords);
+        linearLayoutRecords.removeAllViews();
+        ObjectStudent objectStudent = tableControllerStudent.readSingleRecord(studentId);
+        boolean bool = tableControllerStudent.columnExists(studentId);
+        if (bool == true){
+            String studentFirstname = objectStudent.getFirstname();
+            String studentLastname = objectStudent.getLastname();
+            String studentAddress = objectStudent.getAddress();
+            String studentGender = objectStudent.getGender();
+            String studentCourse = objectStudent.getCourse();
+            String textViewContents = studentId +" "+studentLastname + ", " +studentFirstname + "-\t "+ studentAddress +"-\t\t" + studentGender + "-\t " + studentCourse;
+            TextView textViewStudentItem= new TextView(this);
+            textViewStudentItem.setPadding(4, 16, 4, 16);
+            textViewStudentItem.setTextSize(20);
+            textViewStudentItem.setText(textViewContents);
+            textViewStudentItem.setTag(Integer.toString(studentId));
+            linearLayoutRecords.addView(textViewStudentItem);
+            textViewStudentItem.setOnLongClickListener(new OnClickEditRecord());
+        }
+        else{
+            Toast.makeText(ViewRecord.this, " Student Not found", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+}
